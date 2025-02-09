5752c545395fe0d5c1d996a6648579df90732dd3
RohmatWorkstar <rohmatworkstar99@gmail.com>
2025-02-09 15:35:45 +0700

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { useState } from "react";
 import Userlist from "./pages/Userlist";
 
-type User ={
-  id: number,
-}
-
 function App() {
-  
     return (
         <div className="h-min-screen bg-gradient-to-r to-cyan-500 from-red-300">
             <Userlist />
